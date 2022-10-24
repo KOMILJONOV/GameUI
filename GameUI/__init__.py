eafbacad56639edfb395de92bf9bc6d8c8c4d66b
KOMILJONOV <komiljonovshukurullox@gmail.com>
2022-10-24 14:28:03 +0500

    dfg

--- GameUI/__init__.py
+++ GameUI/__init__.py
@@ -1,47 +1,68 @@
+import sys
 from threading import Thread
+from time import sleep
 import pygame as pg
 from pygame._sdl2 import Window,Renderer
 import signal
+import random
+
+
+
 
 
 
 
 class GameUI(Window):
-    def __init__(self, title:str="GameUI", size: tuple[int]=(640,480),resizeable:bool=True, position: tuple[int]=(0,0),render_fps:int=60, fullscreen:bool=False):
+    threads: list[Thread] = []
+    def __init__(self, title:str="GameUI", size: tuple[int]=(640,480),resizeable:bool=True, position: tuple[int]=(100,100),render_fps:int=60, fullscreen:bool=False):
         super().__init__(title,size,position,fullscreen)
         self.resizable = resizeable
         self.render_fps = render_fps
         self.renderer = Renderer(self)
-        self.clock = pg.time.Clock()
     
     def render(self):
         self.renderer.clear()
-        # for x,y in self.dots:
-        #     self.renderer.draw_color = (255,255,255,255)
-        #     self.renderer.draw_point((x,y))
-        #     self.renderer.draw_color = (0,0,0,0)
+
         self.renderer.present()
+
     
     def loop(self):
         while self.running:
-            self.clock.tick(self.render_fps)
             self.render()
     
     def event_loop(self):
         while self.running:
             events = pg.event.get()
+    
+
+    def onclick(self):
+        for i in range(100):
+            print(i)
+            sleep(0.1)
 
     
     def run(self):
         self.running = True
-        self.render_thread = Thread(target=self.loop)
+        self.render_thread = Thread(target=self.loop,daemon=True)
         self.render_thread.start()
-        self.event_thread = Thread(target=self.event_loop)
-        self.event_thread.start()
-        signal.signal(signal.SIGINT,self.stop)
-        self.render_thread.join()
-        self.event_thread.join()
+        signal.signal(signal.SIGINT, self.stop)
+        while self.running:
+            event = pg.event.wait()
+            # for event in events:
+            if event.type == pg.QUIT:
+                # self.running = False
+                self.stop()
+            if event.type == pg.MOUSEBUTTONUP:
+                thread = Thread(target=self.onclick)
+                thread.setDaemon(True)
+                self.threads.append(thread)
+                thread.start()
+
     
 
     def stop(self,*args):
         self.running = False
+        for thread in self.threads:
+            thread.stop()
+        pg.quit()
+        sys.exit(0)
--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from GameUI import GameUI
 
 
 
-UI = GameUI()
-a = 0
 
-# while True:
-#     UI.title = str(a)
-#     a += 1
-#     UI.render()
+
+
+
+
+UI = GameUI()
 
 
 
